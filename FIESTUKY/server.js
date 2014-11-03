429ef162ce38f95d63ed527bb859c6b021a5eb20
unknown <ik012982i10@206-09-hz109060.H012982.NET>
2014-11-03 08:13:43 +0100

    2.0.1

--- FIESTUKY/server.js
+++ FIESTUKY/server.js
@@ -3,17 +3,17 @@ var mongoose = require('mongoose');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var app = express();
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy =require('passport-local').Strategy;
 
-/*
-var USER=process.env.USER;
-var PASS=process.env.PASS;
-
-*/
+//var user=process.env.USER;
+//var password=process.env.PASSWORD;
 
 
-var user = 'altzi';
-var pass = 'zubiri';
-mongoose.connect('mongodb://'+user+':'+pass+'@ds035240.mongolab.com:35240/usuarios');
+var user = 'aaltzi';
+var password = 'zubiri';
+mongoose.connect('mongodb://'+user+':'+password+'@ds049150.mongolab.com:49150/fiestuky');
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error: '));
@@ -21,16 +21,25 @@ db.once('open', function callback() {
 	console.log('Conecction OK!!!');
 });
 
-
-/*
-passport.serializeUser
-passport.deserializeUser
-*/
 app.configure(function() {
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	app.use(app.router);
 });
+
+/*
+passport.serializeUser
+passport.deserializeUser
+*/
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
 
 // public files
 app.use(express.static(__dirname + '/public'));
@@ -41,7 +50,7 @@ app.get('/', function(req, res){
 
 /*LOG*/
 /*http://danialk.github.io/blog/2013/02/23/authentication-using-passportjs/*/
-var passport = require('passport')
+/*var passport = require('passport')
   , LocalStrategy = require('passport-local').Strategy;
 var FacebookStrategy = require('passport-facebook').Strategy;
 
@@ -65,8 +74,90 @@ passport.use(new LocalStrategy(function(username, password,done){
         });
     });
 }));
-
+*/
 /*************************************************/
+// Recoger usuario local
+passport.use(new LocalStrategy(
+  function(username, pass, done) {
+
+    // Coge el documento de MongoDB
+    console.log(username);
+    console.log(pass);
+
+
+    Users.find({ name: username }, function (err, users) {
+      if (err) return console.error(err);
+      console.log('Find user:');
+      console.log(users);
+
+      // Desglose del usuario encontrado
+      console.log(users[0].pass);
+      console.log(users[0].username);
+
+      var hash = users[0].pass;
+
+      // compara usuario local(username y pass) con el de la base de datos(users.name y .pass) 
+      //if ((username == users.name) && (bcrypt.compareSync(pass, hash))) {
+      if ((username == users[0].name) && (pass==hash)) {
+        // login OK
+        return done(null, username);
+      } else {
+        // login KO
+        console.log("resultados:");
+        console.log("usuario local: "+username);
+        console.log("usuario db: "+users[0].name);
+        console.log("contraseña local: "+pass);
+        console.log("contraseña bd: "+users[0].pass);
+        return done(null, false);
+      }
+
+    });
+  }
+));
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(session({ secret: 'topsecret',
+                  saveUninitialized: true,
+                  resave: true }));
+
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+app.post('/login',
+  passport.authenticate('local', { successRedirect: '/loginSuccess',
+                                   failureRedirect: '/loginFailure',
+                                   failureFlash: false })
+);
+
+app.get('/', function(req,res) {
+  res.redirect('index.html');
+});
+
+app.get('/loginFailure', function(req,res) {
+  res.send('Login KO. username/pass incorrect');
+});
+
+
+app.get('/loginSuccess', ensureAuthenticated, function(req,res) {
+  res.send('Login OK. Hello ' + req.user);
+});
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/');
+}
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
 
 
 /*Usuario*/
@@ -79,7 +170,7 @@ var usuario = new Schema({
   mail: String
 });
 
-module.exports = mongoose.model('Usuario', usuario);
+var Users = mongoose.model('Users',usuario);
 
 /***************************************************/
 
