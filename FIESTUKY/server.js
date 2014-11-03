5b422a58714e3b5cb447e3a1782d2b0d0bd2f7f4
unknown <ik012982i10@206-09-hz109060.H012982.NET>
2014-11-03 12:41:18 +0100

    v1.2.2

--- FIESTUKY/server.js
+++ FIESTUKY/server.js
@@ -7,12 +7,12 @@ var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy =require('passport-local').Strategy;
 
-//var user=process.env.USER;
-//var password=process.env.PASSWORD;
+var user=process.env.USER;
+var password=process.env.PASSWORD;
 
 
-var user = 'aaltzi';
-var password = 'zubiri';
+//var user = 'aaltzi';
+//var password = 'zubiri';
 mongoose.connect('mongodb://'+user+':'+password+'@ds049150.mongolab.com:49150/fiestuky');
 var db = mongoose.connection;
 
@@ -40,6 +40,17 @@ passport.deserializeUser(function(user, done) {
   done(null, user);
 });
 
+/*Usuario*/
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+var usuario = new Schema({
+  username: String,
+  pass: String,
+  mail: String
+});
+
+var Users = mongoose.model('Users',usuario);
 
 // public files
 app.use(express.static(__dirname + '/public'));
@@ -54,14 +65,14 @@ app.get('/', function(req, res){
 /*************************************************/
 // Recoger usuario local
 passport.use(new LocalStrategy(
-  function(username, pass, done) {
+  function(username, password, done) {
 
-    // Coge el documento de MongoDB
-    console.log(username);
-    console.log(pass);
+    // usuario local
+    console.log(user);
+    console.log(password);
 
 
-    Users.find({ name: username }, function (err, users) {
+    Users.find({ username : user }, function (err, users) {
       if (err) return console.error(err);
       console.log('Find user:');
       console.log(users);
@@ -72,17 +83,17 @@ passport.use(new LocalStrategy(
 
       var hash = users[0].pass;
 
-      // compara usuario local(username y pass) con el de la base de datos(users.name y .pass) 
-      //if ((username == users.name) && (bcrypt.compareSync(pass, hash))) {
-      if ((username == users[0].name) && (pass==hash)) {
+      // compara usuario local(username y pass) con el de la base de datos(Users.name y .pass) 
+      //if ((username == Users.name) && (bcrypt.compareSync(pass, hash))) {
+      if ((user == users[0].username) && (password==hash)) {
         // login OK
-        return done(null, username);
+        return done(null, user);
       } else {
         // login KO
         console.log("resultados:");
-        console.log("usuario local: "+username);
-        console.log("usuario db: "+users[0].name);
-        console.log("contraseña local: "+pass);
+        console.log("usuario local: "+user);
+        console.log("usuario db: "+users[0].username);
+        console.log("contraseña local: "+password);
         console.log("contraseña bd: "+users[0].pass);
         return done(null, false);
       }
@@ -135,26 +146,12 @@ app.get('/logout', function(req, res){
   res.redirect('/');
 });
 
-
-/*Usuario*/
-var mongoose = require('mongoose'),
-  Schema = mongoose.Schema;
-
-var usuario = new Schema({
-  username: String,
-  pass: String,
-  mail: String
-});
-
-var Users = mongoose.model('Users',usuario);
-
 /***************************************************/
 
-/*
 app.get('/login', function(req, res){
   res.redirect('pagina.html');
 });
-*/
+
 require('./routes')(app);
 
 app.listen(5000);
