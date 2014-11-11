ff60609f3a1c93671cb262ee5ed86dd89e565266
aaltzi <aaltzibar007@gmail.com>
2014-11-11 18:09:46 +0100

    ultimo

--- FIESTUKY/server.js
+++ FIESTUKY/server.js
@@ -1,3 +1,4 @@
+// DEPENDENCIAS
 var express = require('express');
 var mongoose = require('mongoose');
 var cookieParser = require('cookie-parser');
@@ -8,20 +9,18 @@ var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy =require('passport-local').Strategy;
 
+// CONEXIÓN BASE DE DATOS
 var user=process.env.USER;
 var password=process.env.PASSWORD;
-
-
-//var user = 'aaltzi';
-//var password = 'zubiri';
 mongoose.connect('mongodb://'+user+':'+password+'@ds049150.mongolab.com:49150/fiestuky');
 var db = mongoose.connection;
-
+// comprobar conexión
 db.on('error', console.error.bind(console, 'connection error: '));
 db.once('open', function callback() {
 	console.log('Conecction OK!!!');
 });
 
+// Configuración de las dependencias...
 app.configure(function() {
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
@@ -29,33 +28,31 @@ app.configure(function() {
   app.use(passport.session()); 
 	app.use(app.router);
   app.use(bodyParser.urlencoded({ extended: false }));
-  // public files
+  // aceso a la carpeta public para las imagenes y los html
   app.use(express.static(__dirname + '/public'));
+  // Enciptacion de sesion
   app.use(session({ secret: 'topsecret',
                   saveUninitialized: true,
                   resave: true }));
   });
 
-  passport.serializeUser(function(user, done) {
-    done(null, user);
-  });
-
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
-
+// La esquema para la base de datos de los usuarios
 var usuario = new mongoose.Schema({
   name: String,
   password: String
 },{ collection : 'Users' });
 
+// Crear la variable de la esquema del usuario
 var Users = mongoose.model('Users',usuario);
 
-app.get('/', function(req, res){
-	res.redirect('index.html');
+// Passport 
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+  done(null, user);
 });
 
-/*************************************************/
 // Recoger usuario local
 passport.use(new LocalStrategy(
   function(username, password, done) {
@@ -64,25 +61,24 @@ passport.use(new LocalStrategy(
     console.log(username);
     console.log(password);
 
-
+    // Comparar el ususario introducido con el de la base de datos
     Users.find({ name: username}, function (err, users) {
       if (err) return console.error(err);
+      
+      // Comprobaciones por consola
       console.log('Find user:');
       console.log(users);
-
-      // Desglose del usuario encontrado
       console.log(users[0].password);
       console.log(users[0].name);
-
+      // Recoger la contraseña en una variable
       var hash = users[0].password;
-
-      // compara usuario local(username y pass) con el de la base de datos(Users.name y .pass) 
-      //if ((username == Users.name) && (bcrypt.compareSync(pass, hash))) {
+      // Comparar el usuario de la base de datos y el que se ha introducido
       if ((username == users[0].name) && (password==hash)) {
-        // login OK
+        // login Bien
         return done(null, username);
       } else {
-        // login KO
+        // login Mal
+        // Comprobaciones
         console.log("resultados:");
         console.log("usuario local: "+username);
         console.log("usuario db: "+users[0].name);
@@ -90,55 +86,42 @@ passport.use(new LocalStrategy(
         console.log("contraseña bd: "+users[0].password);
         return done(null, false);
       }
-
     });
   }
 ));
 
-
-
-
+// Accion al hacer el login
 app.post('/login',
   passport.authenticate('local', { successRedirect: '/loginSuccess',
                                    failureRedirect: '/loginFailure',
                                    failureFlash: false })
 );
-
-app.get('/', function(req,res) {
-  res.redirect('index.html');
-});
-
+// Login bien o mal
 app.get('/loginFailure', function(req,res) {
   res.send('Login KO. username/pass incorrect');
 });
-
-
 app.get('/loginSuccess', ensureAuthenticated, function(req,res) {
   res.send('Login OK!!!');
 });
 
-// Simple route middleware to ensure user is authenticated.
-//   Use this route middleware on any resource that needs to be protected.  If
-//   the request is authenticated (typically via a persistent login session),
-//   the request will proceed.  Otherwise, the user will be redirected to the
-//   login page.
+// Comprobar la autentificacion del usuario para que no
+// se pueda acceder de otra manera
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
   res.redirect('/pagina.html');
 }
-
+// Redirecciones
+// Salir de la sesion ...¿?
 app.get('/logout', function(req, res){
   req.logout();
   res.redirect('/');
 });
-
-/***************************************************/
-
+app.get('/', function(req,res) {
+  res.redirect('index.html');
+});
 app.get('/login', function(req, res){
   res.redirect('pagina.html');
 });
-
-require('./routes')(app);
-
+// Escuchar en el puerto 5000 el servidor Express
 app.listen(5000);
 console.log('Servidor Express escuchando en el puerto 5000');
